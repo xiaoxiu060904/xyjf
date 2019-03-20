e36ac982f3f063eb962e2bc4df9e324dce90cc1c
ppd-02020445 <xiaoqiang4776@126.com>
2019-03-20 19:09:11 +0800

    init

---
+++ src/main/java/com/xiao/test/web/page/Hs.java
@@ -0,0 +1,22 @@
+package com.xiao.test.web.page;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * desc 健康页
+ *
+ * @author xiaoqiang
+ * @data 2019/3/20
+ */
+@RequestMapping
+@RestController
+public class Hs {
+    @PostMapping("/hs")
+    @ApiOperation("健康页")
+    public String hs() {
+        return "OK";
+    }
+}
