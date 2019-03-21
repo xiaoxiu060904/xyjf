035fd2145e39bc110cbeb14da7191d53393a2ba0
ppd-02020445 <xiaoqiang4776@126.com>
2019-03-21 17:11:36 +0800

    还原

--- src/main/java/com/xiao/test/Bootstrap.java
+++ src/main/java/com/xiao/test/Bootstrap.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -16,7 +17,7 @@ import org.springframework.context.annotation.Bean;
  **/
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
 public class Bootstrap {
