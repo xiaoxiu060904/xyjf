4038f313988a2142b02d63e9eccc96abf24809c7
ppd-02020445 <xiaoqiang4776@126.com>
2019-08-19 20:00:44 +0800

    pom 修改

--- src/main/java/com/xiao/test/Bootstrap.java
+++ src/main/java/com/xiao/test/Bootstrap.java
@@ -3,9 +3,7 @@ package com.xiao.test;
 import feign.Request;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -17,9 +15,7 @@ import org.springframework.context.annotation.Bean;
  **/
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
-@EnableCircuitBreaker
 public class Bootstrap {
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class,args);
